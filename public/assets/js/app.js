c1db483edc0a85444577c8d47e11d42d91f4555c
nancyzj <ancizj@hotmail.com>
2017-07-18 16:44:52 -0500

    mejoras en la pantalla principal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,9 +2,7 @@
 	function cargarPagina(){
 		// alert('hola');
 		var carusel = $('.carousel.carousel-slider').carousel({fullWidth: true});
-		carusel.css({"width": "100%", "height": "300px"});
-
-		// $('.carousel').carousel();
+		carusel.css({"width": "100%", "height": "500px"});
 	}
 
 	$(document).ready(cargarPagina);
