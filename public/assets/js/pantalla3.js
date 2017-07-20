e7d0fe97220534d47302e7a9d4ee408d628dda6d
nancyzj <ancizj@hotmail.com>
2017-07-20 05:10:24 -0500

    funcionalidad en pantalla 3

--- public/assets/js/pantalla3.js
+++ public/assets/js/pantalla3.js
@@ -1,13 +1,33 @@
 (function() {
-		// swal("pantalla3");
+	var inputCodigo = $('#autocomplete-input');
+	var obtenerCodigo;
 	var cargarPagina = function(){
 		var obtenerNumero = localStorage.getItem('numeroCel');
-		// console.log(obtenerNumero);
 		$('#numero').text(obtenerNumero);
 		// obtener codigo generado
-		var obtenerCodigo = localStorage.getItem('codigoGenerado');
-		// console.log(obtenerNumero);
+		obtenerCodigo = localStorage.getItem('codigoGenerado');
+		console.log(obtenerCodigo);
 		// swal("Bienvenido", "Tu codigo es: " + obtenerCodigo, "success")
+		inputCodigo.keyup(validarCodigo);
+		inputCodigo.keydown(validarNumeros);
+	}
+	var validarCodigo = function(){
+		var arreglo = [];
+		arreglo.push(inputCodigo.val());
+		var serie = arreglo.join('');
+		if (serie == obtenerCodigo){
+			location.href = "/view/pantalla2.html";
+			// console.log('ok');
+		}//else{
+		// 	sweetAlert("Oops...", "Codigo incorrecto", "error");
+		// 	// console.log('no');
+		// }
+		// console.log(typeof serie);
+	}
+	var validarNumeros = function(e){
+		if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57)){
+			e.preventDefault();
+		}	
 	}
 	$(document).ready(cargarPagina);
 })();
