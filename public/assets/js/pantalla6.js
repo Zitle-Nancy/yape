451024a88384a127378194051a5e67027a56eb98
nancyzj <ancizj@hotmail.com>
2017-07-21 10:40:31 -0500

    version

--- public/assets/js/pantalla6.js
+++ public/assets/js/pantalla6.js
@@ -1,8 +1,13 @@
 (function() {
+	var botonSiguiente = $('#btn-siguiente');
 	var cargarPagina = function(){
 		$(document).ready(function() {
 			$('select').material_select();
 		});
+		botonSiguiente.click(validarTarjeta);
+	}
+	var validarTarjeta = function(){
+		location.href = '/view/pantalla7.html'
 	}
 	$(document).ready(cargarPagina);
 })();
