d9813ceeb5bff75e3091cb03fe7f7b753d5d80bc
nancyzj <ancizj@hotmail.com>
2017-07-22 00:35:48 -0500

    obtener valores del select

--- public/assets/js/pantalla6.js
+++ public/assets/js/pantalla6.js
@@ -1,13 +1,28 @@
 (function() {
 	var botonSiguiente = $('#btn-siguiente');
 	var inputTarejta = $('#autocomplete-input');
+	var selectMes = $('#mes');
+	var selectAnio = $('#anio');
+
 	var cargarPagina = function(){
 		$('select').material_select();
 		botonSiguiente.click(validarTarjeta);
 		inputTarejta.keydown(validarNumeros);
+		selectMes.change(obtenerMes);
+		selectAnio.change(obtenerAnio);
 	};
 	var validarTarjeta = function(){
 		location.href = '/view/pantalla7.html'
-	}
+	};
+	var obtenerMes = function(){
+		var valorMes = $('#mes option:selected').html();
+		console.log(valorMes);
+		// console.log(this);
+		// console.log(valorMes);
+	};
+	var obtenerAnio = function(){
+		var valorAnio = $('#anio option:selected').html();
+		console.log(valorAnio);
+	};
 	$(document).ready(cargarPagina);
 })();
