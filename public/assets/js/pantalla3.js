367d04d62bb6bce1077d6ed36c6b5848f11bf137
nancyzj <ancizj@hotmail.com>
2017-07-23 18:14:51 -0500

    generar codigo usando el api

--- public/assets/js/pantalla3.js
+++ public/assets/js/pantalla3.js
@@ -1,7 +1,7 @@
 (function() {
 	var inputCodigo = $('#autocomplete-input');
 	var obtenerCodigo;
-	var numero = 22;
+	var numero = 7;
 	var cargarPagina = function(){
 		var obtenerNumero = localStorage.getItem('numeroCel');
 		$('#numero').text(obtenerNumero);
@@ -11,12 +11,12 @@
 		swal("Bienvenido", "Tu codigo es: " + obtenerCodigo, "success")
 		inputCodigo.keyup(validarCodigo);
 		inputCodigo.keydown(validarNumeros);
+		contador();
 	};
 	var validarCodigo = function(){
-		var arreglo = [];
-		arreglo.push(inputCodigo.val());
-		var serie = arreglo.join('');
-		var longitud = inputCodigo.val().trim().length;
+		// un input siempre devuelve un string
+		var serie = inputCodigo.val();
+		var longitud = serie.trim().length;
 		console.log(longitud);
 		if(obtenerCodigo.length == longitud){
 			if (serie == obtenerCodigo){
@@ -29,20 +29,39 @@
 				inputCodigo.val('');
 			}
 		}
-		// limpiar input
-		
 	};
 	var contador = function(){
 		myVar = setInterval(decrementar, 1000);
 	};
 	var decrementar = function(){
 		numero--;
+		var obtenerNumero = localStorage.getItem('numeroCel');
 		if(numero == 0){
+			numero = 21;
 			clearInterval(myVar);
+			// api //
+			$.post('http://localhost:3000/api/resendCode',
+			{
+				"phone": obtenerNumero
+			}).then(function(response){
+				// then para saber si es usuario valido
+				var mensaje = response.message;
+				if(response.success){
+					// obtenemos codigo
+					obtenerCodigo = response.data.toString();
+					console.log(obtenerCodigo);
+					swal("Tu nuevo", "codigo es: " + obtenerCodigo,"success");
+					contador();
+				}else{
+					sweetAlert("Oops...",mensaje, "error");
+				}
+			}).catch(function(error){
+				console.log(error);
+			});
+			// fin de api //
 		}
 		$('#incremento').text(numero);
 		console.log(numero);
 	}
-	contador();
 	$(document).ready(cargarPagina);
 })();
